4c7e85195e380efd2584f9781faa964a9af7a9ac
Nino Aguilar <12245781+ninoaguilar@users.noreply.github.com>
2022-10-24 15:07:11 -0600

    Implemented custom logging interceptor

--- src/main/java/com/example/rqchallenge/RqChallengeApplication.java
+++ src/main/java/com/example/rqchallenge/RqChallengeApplication.java
@@ -1,7 +1,9 @@
 package com.example.rqchallenge;
 
+import com.example.rqchallenge.utils.CustomRestTemplateCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RqChallengeApplication {
@@ -10,4 +12,8 @@ public class RqChallengeApplication {
         SpringApplication.run(RqChallengeApplication.class, args);
     }
 
+    @Bean
+    public CustomRestTemplateCustomizer customRestTemplateCustomizer() {
+        return new CustomRestTemplateCustomizer();
+    }
 }
